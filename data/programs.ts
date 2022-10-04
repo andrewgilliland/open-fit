3786e2e6e3c8d5bd0fa3a772cea69c1c11b66210
Andrew Gilliland <andrew.gilliland@csm.cc>
2022-10-03 22:02:09 -0500

    feat: ui stuff

---
+++ data/programs.ts
@@ -0,0 +1,88 @@
+export const programOne = {
+  name: 'PLPL',
+  workouts: [
+    {
+      day: 1,
+      name: 'Pull Day',
+      exercises: [
+        {
+          name: 'Lat Pulldown',
+          targetGroup: 'Back',
+          sets: [15, 12, 10, 8],
+        },
+        {
+          name: 'Cable Row',
+          targetGroup: 'Back',
+          sets: [15, 12, 10, 8],
+        },
+        {
+          name: 'Face Pull',
+          targetGroup: 'Traps',
+          sets: [15, 12, 10, 8],
+        },
+        {
+          name: 'Lateral DB Raise',
+          targetGroup: 'Delts',
+          sets: [15, 15, 15, 15],
+        },
+        {
+          name: 'Cable Curl',
+          targetGroup: 'Latissimus Dorsi',
+          sets: [15, 15, 15, 15],
+        },
+        {
+          name: 'Lying Leg Raises',
+          targetGroup: 'Latissimus Dorsi',
+          sets: [15, 15, 15, 15],
+        },
+        { day: 2 },
+        { day: 3 },
+        { day: 4 },
+      ],
+    },
+  ],
+}
+
+export const programTwo = {
+  name: 'PPL',
+  workouts: [
+    {
+      day: 1,
+      name: 'Pull Day',
+      exercises: [
+        {
+          name: 'Lat Pulldown',
+          targetGroup: 'Back',
+          sets: [15, 12, 10, 8],
+        },
+        {
+          name: 'Cable Row',
+          targetGroup: 'Back',
+          sets: [15, 12, 10, 8],
+        },
+        {
+          name: 'Face Pull',
+          targetGroup: 'Traps',
+          sets: [15, 12, 10, 8],
+        },
+        {
+          name: 'Lateral DB Raise',
+          targetGroup: 'Delts',
+          sets: [15, 15, 15, 15],
+        },
+        {
+          name: 'Cable Curl',
+          targetGroup: 'Latissimus Dorsi',
+          sets: [15, 15, 15, 15],
+        },
+        {
+          name: 'Lying Leg Raises',
+          targetGroup: 'Latissimus Dorsi',
+          sets: [15, 15, 15, 15],
+        },
+      ],
+    },
+    { day: 2 },
+    { day: 3 },
+  ],
+}
